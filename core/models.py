dcf26a4aec7b5b215ab29a640f1133e59c21a462
aigul <halitovaajgul210@gmail.com>
2024-10-16 17:18:26 +0500

    работает список продуктов

--- core/models.py
+++ core/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 from core import consts
--- core/views.py
+++ core/views.py
@@ -1,2 +1,18 @@
+from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+from core.models import Product
+
+
+class FirstView(View):
+    def get(self, request):
+        return HttpResponse(' Hi Django!')
+
+
+class ListView(TemplateView):
+    template_name = 'produce_shelf/produce_shelf.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produce'] = Product.objects.all()
+        return context
