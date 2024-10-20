f0ccf3702654d80674ee030fe1e2e07084a65535
aigul <halitovaajgul210@gmail.com>
2024-10-20 18:04:27 +0500

    Сделано

--- README.md
+++ README.md
@@ -33,6 +33,18 @@ BooleanFilter по заказанным товарам
 
 ![Заказанные товары.png](Screenshots%2Ffilters%2F%D0%97%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%82%D0%BE%D0%B2%D0%B0%D1%80%D1%8B.png)
 
+Поиск по названию и отзыву через Q запрос
+
+![Поиск по названию и отзыву через Q запрос.png](Screenshots%2Ffilters%2F%D0%9F%D0%BE%D0%B8%D1%81%D0%BA%20%D0%BF%D0%BE%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E%20%D0%B8%20%D0%BE%D1%82%D0%B7%D1%8B%D0%B2%D1%83%20%D1%87%D0%B5%D1%80%D0%B5%D0%B7%20Q%20%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81.png)
+
+![Обновленный перечень отзывов.png](Screenshots%2Ffilters%2F%D0%9E%D0%B1%D0%BD%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9%20%D0%BF%D0%B5%D1%80%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%20%D0%BE%D1%82%D0%B7%D1%8B%D0%B2%D0%BE%D0%B2.png)
+
+
+
+
+
+
+
 
 # Домашнее задание 4
 
--- core/filters.py
+++ core/filters.py
@@ -1,17 +1,19 @@
 import django_filters
 import core.models
+from django.db.models import Q
 
 
 class Product(django_filters.FilterSet):
-    price_range = django_filters.RangeFilter(field_name='price', label='Price of order')
-    name = django_filters.CharFilter(lookup_expr='icontains', label='Name')
+    # price_range = django_filters.RangeFilter(field_name='price', label='Price of order')
+    # name = django_filters.CharFilter(lookup_expr='icontains', label='Name')
     comment = django_filters.CharFilter(field_name='reviews__comment', lookup_expr='icontains',
                                         label='Comment contains')
     ordered = django_filters.BooleanFilter(method='InOrder', label='In order')
+    term = django_filters.CharFilter(method='Contains_in', label='')
 
     class Meta:
         model = core.models.Product
-        fields = ['name', 'price_range', 'comment', 'ordered']
+        fields = ['comment', 'ordered', 'term']
 
     def InOrder(self, queryset, name, value):
         if value is None:
@@ -19,3 +21,9 @@ class Product(django_filters.FilterSet):
         if value:
             return queryset.filter(details__order__isnull=False).distinct()
         return queryset.exclude(details__order__isnull=False).distinct()
+
+    def Contains_in(self, queryset, name, value):
+        criterias = Q()
+        for t in value.split():
+            criterias &= Q(name__icontains=t) | Q(reviews__comment__icontains=t)
+        return queryset.filter(criterias).distinct()
